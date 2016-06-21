31b86c67aaaf6a9681637b6c8307be957373a881
Ascendzor <wow.tmare@gmail.com>
2016-06-21 15:16:24 +1200

    loading pirate base map

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import leaflet from 'leaflet'
-var mymap = leaflet.map('map').setView([51.505, -0.09], 13);
+var map = leaflet.map('map', {
+  zoom: 5,
+  attributionControl: false
+}).setView([-20, 160], 2)
+
+var baseMap = leaflet.tileLayer('https://c.tiles.mapbox.com/v4/aj.Sketchy2/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWV0b2NlYW4iLCJhIjoia1hXZjVfSSJ9.rQPq6XLE0VhVPtcD9Cfw6A', {
+    maxZoom: 18
+}).addTo(map)
 
 import React from 'react'
 import ReactDOM from 'react-dom'
