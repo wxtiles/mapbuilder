9e79626ba4305c12a536e0ce503cbe199005ee08
Mitchell Burton <m.burton@metocean.co.nz>
2016-06-22 13:34:53 +1200

    Load all the maps at once and just hide that ones we ae not currenly using.

--- index.js
+++ index.js
@@ -5,26 +5,20 @@ import leaflet from 'leaflet'
 import createTileLayer from './createTileLayer'
 import mapSelector from './mapSelector'
 
-var clearMapContainer = () => {
-  //Kill the map div and replace it with a black one so we can re-mount a map into a fresh container.
-  document.getElementById('map').outerHTML = "<div id='map'></div>";
-}
+var leafletMap, googleMap;
 
 var mountLeafletMap = () => {
-  clearMapContainer()
-  if (map != undefined) { map.remove(); }
-  var map = leaflet.map('map', {
+  leafletMap = leaflet.map('leafletMap', {
     zoom: 5,
     attributionControl: false
   }).setView([-20, 160], 2)
 
   var baseMap = leaflet.tileLayer('https://c.tiles.mapbox.com/v4/aj.Sketchy2/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWV0b2NlYW4iLCJhIjoia1hXZjVfSSJ9.rQPq6XLE0VhVPtcD9Cfw6A', {
     maxZoom: 18
-  }).addTo(map)
+  }).addTo(leafletMap)
 }
 var mountGoogleMap = () => {
-  clearMapContainer()  
-  var googleMap = new google.maps.Map(document.getElementById('map'), {
+  googleMap = new google.maps.Map(document.getElementById('googleMap'), {
     center: {
       lat: 39.50,
       lng: -98.35
@@ -33,22 +27,35 @@ var mountGoogleMap = () => {
   });
 }
 
+var showMap = (selectedMap) => {
+  document.getElementById('leafletMap').style.display = 'none';
+  document.getElementById('googleMap').style.display = 'none';
+  document.getElementById('mapBoxGLMap').style.display = 'none';
+
+  document.getElementById(`${selectedMap.value}Map`).style.display = 'block';
+}
+
 var reactMount = document.querySelector('#interface')
 
 var mapSelectorMount = document.querySelector('#mapSelector')
 var examples = [
-  { label: 'Leaflet', value: 'leaflet', mountMap: mountLeafletMap },
-  { label: 'Google Maps', value: 'googleMaps', mountMap: mountGoogleMap },
-  { label: 'Map Box GL', value: 'mapBoxGL', mountMap: () => console.log('mounting map box') },
+  { label: 'Leaflet', value: 'leaflet' },
+  { label: 'Google Maps', value: 'google' },
+  { label: 'Map Box GL', value: 'mapBoxGL' },
 ]
 
-ReactDOM.render(React.createElement(mapSelector, { options: examples }), mapSelectorMount)
+ReactDOM.render(React.createElement(mapSelector, { options: examples, showMap: showMap }), mapSelectorMount)
+
+//Prepare the maps.
+mountLeafletMap()
+mountGoogleMap()
 
 var putLayer = (url) => {
+  console.log(url)
   leaflet.tileLayer(url, {
       maxZoom: 18,
       tms: true
-  }).addTo(map)
+  }).addTo(leafletMap)
 }
 
 ReactDOM.render(React.createElement(createTileLayer, {putLayer}), reactMount)
---
+++ mapSelector.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import select from 'react-select'
+
+class mapSelector extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+    this.state.selectedMap = null;
+  }
+
+  render() {
+    return React.createElement(`div`, {className: 'mapSelector'},
+      React.createElement('div', null, 'Pick an example'),
+      React.createElement(select, {
+        options: this.props.options,
+        value: this.state.selectedMap,
+        onChange: (selectedMap) => {
+          this.setState({selectedMap})
+          this.props.showMap(selectedMap)
+        }
+
+      })
+    )
+  }
+}
+
+export default mapSelector
