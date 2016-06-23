c01b35896d7a31ec87f7d93dd693410a9bfed88f
Mitchell Burton <mitchell.burton@gmail.com>
2016-06-23 14:11:54 +1200

    Add layer to Google Maps (WIP).

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import root from './root'
 import createTileLayer from './createTileLayer'
 import mapSelector from './mapSelector/mapSelector'
 import maps from './maps'
+import leaflet from 'leaflet'
 
 //Prepare the maps.
 var mapExamples = [
@@ -14,6 +15,7 @@ var mapExamples = [
 
 var leafletMap = maps.mountLeafletMap();
 var googleMap = maps.mountGoogleMap();
+var currentLayerUrl = '';
 
 var defaultMap = mapExamples[0]
 
@@ -22,8 +24,20 @@ ReactDOM.render(React.createElement(mapSelector, { mapOptions: mapExamples, show
 
 maps.showMap(defaultMap)
 
+var mapTiler = new google.maps.ImageMapType({ 
+  getTileUrl: (coord, zoom) => {
+    return currentLayerUrl.replace('{z}', zoom).replace('{x}', coord.x).replace('{y}', (Math.pow(2,zoom)-coord.y-1) ); 
+    //return zoom + "/" +  + "/" + (Math.pow(2,zoom)-coord.y-1) + ".png"; 
+}, 
+  tileSize: new google.maps.Size(256, 256), 
+  isPng: true
+}); 
+
+googleMap.overlayMapTypes.insertAt(0, mapTiler);
+
 var putLayer = (url) => {
   console.log(url)
+  currentLayerUrl = url;
   leaflet.tileLayer(url, {
       maxZoom: 18,
       tms: true
