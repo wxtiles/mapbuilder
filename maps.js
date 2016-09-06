fd5af80b13da63ef4c8d467060f32d6073db0acf
Ascendzor <wow.tmare@gmail.com>
2016-09-07 11:01:27 +1200

    swapped basemaps for leaflet and openlayers

--- maps.js
+++ maps.js
@@ -10,9 +10,9 @@ maps.mountLeafletMap = () => {
     attributionControl: false
   }).setView([-20, 160], 2)
 
-  var baseMap = leaflet.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
+  var baseMap = leaflet.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    subdomains: 'abcd',
+    subdomains: 'abc',
     minZoom: 0,
     maxZoom: 20,
   }).addTo(leafletMap)
@@ -43,7 +43,7 @@ var openLayersBaseMap = new ol.layer.Tile({
         html: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
       })
     ],
-    url: 'http://{a-c}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'
+    url: 'http://stamen-tiles-{a-d}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png'
   })
 });
 
