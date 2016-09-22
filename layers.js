edf16eb93edfd6e0eef9bef6eb6bd00c5511402b
Ascendzor <wow.tmare@gmail.com>
2016-09-22 15:47:48 +1200

    layer ordering now totally works

--- layers.js
+++ layers.js
@@ -11,18 +11,14 @@ class layers extends React.Component {
     this.state.layers = []
     this.state.totalLayers = 0
     this.state.shouldShowLayerMenu = true
-    this.state.allLayerIds = []
     this.state.isMakingUrl = false
+    this.drake = null
 
     this.addLayerSelectionRow = this.addLayerSelectionRow.bind(this)
     this.createLayer = this.createLayer.bind(this)
     this.removeLayer = this.removeLayer.bind(this)
     this.toggleLayerMenu = this.toggleLayerMenu.bind(this)
     this.setOpacityOfLayer = this.setOpacityOfLayer.bind(this)
-  }
-
-  componentWillMount() {
-    this.addLayerSelectionRow()
   }
 
   componentDidMount() {
@@ -34,12 +30,13 @@ class layers extends React.Component {
 
     drake.on('drop', (el, target, source, sibling) => {
       var layersInDom = drake.containers[0].children
+      var layersState = this.state.layers
       var layers = _.map(this.state.layers, (layer) => {
         if(!layer) return undefined
         var indexOfLayer = _.findIndex(layersInDom, (layerDom) => {
           return layerDom.attributes[1].nodeValue == layer.key
         })
-        layer.zIndex = (100 - indexOfLayer)
+        layer.zIndex = layersInDom.length - indexOfLayer
         return layer
       })
       this.setState({layers: layers})
@@ -48,15 +45,19 @@ class layers extends React.Component {
         this.props.updateLayers({layerKey: layer.key, layerObject: layer})
       })
     })
+    this.setState({drake}, () => {
+      this.addLayerSelectionRow()
+    })
   }
 
   //This is called when the user clicks the button to add a new layer.
   addLayerSelectionRow() {
     var allLayers = this.state.layers
-    allLayers[this.state.totalLayers] = {
+    allLayers.unshift({
       key: this.state.totalLayers,
       zIndex: this.state.totalLayers
-    }
+    })
+
     this.setState({layers: allLayers, totalLayers: this.state.totalLayers+1})
     //createLayer will be called at the end of this chain, after all the data has come back from the server.
   }
@@ -64,15 +65,12 @@ class layers extends React.Component {
   //This is called when the use selects a time value for the layer.
   //This also happens once when the slayer selection row is first loaded, the 0th time value is auto selected for the user.
   createLayer({layerKey, url, layerObject}) {
-    var allLayerIds = this.state.allLayerIds
-    allLayerIds.push(layerObject.id)
     var layers = this.state.layers
-    layerObject.zIndex = layers[layerKey].zIndex
-    if (!layerObject.zIndex) layerObject.zIndex = (100 - layerKey)
-    layers[layerKey] = layerObject
-    this.setState({allLayerIds, layers})
+    var index = _.findIndex(layers, (layer) => layer.key == layerObject.key)
+    layerObject.zIndex = layers[index].zIndex
+    layers[index] = layerObject
+    this.setState({layers: layers})
     this.props.putLayer(layerKey, url, layerObject.zIndex)
-    this.props.updateLayers({layerKey, layerObject})
   }
 
   removeLayer({layerKey}) {
