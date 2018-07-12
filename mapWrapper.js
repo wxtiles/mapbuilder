620405bafceaf98b8170313e75d48bc47174dc9e
Richard Law <richard.m.law@gmail.com>
2018-07-12 15:35:43 +1200

    Update mapWrapper.js
    http → https

--- mapWrapper.js
+++ mapWrapper.js
@@ -146,7 +146,7 @@ class mapWrapper extends React.Component {
           zIndex: 500
         }),
         React.createElement(TileLayer, {
-          url: 'http://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png',
+          url: 'https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png',
           subdomains: 'abcd',
           zIndex: 501,
           alpha: 1
